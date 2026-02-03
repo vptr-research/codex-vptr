218015262aba1f4b3158b1313daca4b8743df0e0
vptr <vptr@fastmail.com>
2026-02-02 21:02:34 -0300

    [Phase 1] ft_strlen: add edge case tests

--- projects/libft/tests/test_ft_strlen.c
+++ projects/libft/tests/test_ft_strlen.c
@@ -5,13 +5,17 @@ size_t ft_strlen(const char *s);
 
 int main()
 {
-	const char *phrase = "Test Strlen";
-	int got = ft_strlen(phrase);
-	int expected = strlen(phrase);
-		if (got != expected)
-			write (1, "Incorrect Implementation\n", 25);
-		else
-			write (1, "Correct Implementation\n", 23);
+	const char *phrase[] = {"Test Strlen", "", "     ", "\n", "a"};
+	int i = 0;
+		while ( i < 5)
+		{
+			size_t got = ft_strlen(phrase[i]);
+			size_t expected = strlen(phrase[i]);
+			if (got != expected)
+				write (1, "Incorrect Implementation\n", 25);
+			else
+				write (1, "Correct Implementation\n", 23);
+		i++;
+		}
 	return (0);
 }
-
